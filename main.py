d2f597cb9ea1951dca7ab230db30e71631b564fa
Mike <ms53150@gmail.com>
2022-05-20 13:08:47 -0400

    Send company information to email after checking stock prices

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import requests
+import smtplib
+
+my_email = "pythonemailtest106"
+password = os.environ.get("PASSWORD")
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -16,52 +20,47 @@ stock_params = {
     "apikey": stock_api_key
 }
 
-news_params = {
-    "q": COMPANY_NAME,
-    "apikey": news_api_key
-}
-
-
 stock_response = requests.get(STOCK_ENDPOINT, params=stock_params)
 stock_response.raise_for_status()
 stock_data = stock_response.json()["Time Series (Daily)"]
-
-news_response = requests.get(NEWS_ENDPOINT, params=news_params)
-news_response.raise_for_status()
-
 
 stock_list = [value for (key, value) in stock_data.items()]
 yesterday_data = stock_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 
-
 day_before_yesterday_data = stock_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 
 if diff_percent > .01:
-    print("Get News")
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+    news_params = {
+        "q": COMPANY_NAME,
+        "apikey": news_api_key
+    }
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    three_articles = articles[:3]
 
+    formatted_articles = [f"Headline: {article['title']}: {up_down}{diff_percent}. \nBrief: {article['description']}"
+                          for article in three_articles]
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        for article in formatted_articles:
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="tonystark53150@gmail.com",
+                                msg=f"Subject: Tesla Stock Info \n\n{formatted_articles}".encode('utf-8'))
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -71,4 +70,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
